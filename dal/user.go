fde38ab40226b73500f6664fc43409191bcccb5f
korojy <ali.korojy@gmail.com>
2021-12-17 12:21:29 +0330

    clean code

--- dal/user.go
+++ dal/user.go
@@ -21,23 +21,6 @@ func CreateUser(userSignup DAO.UserSignup) DAO.User {
 	return user
 }
 
-func ChangePassword(user DAO.User, password string) bool {
-	hash, err := DAO.HashPassword(password)
-	if err != nil {
-		return false
-	}
-	config.DB.Model(&user).Update(DAO.User{Password: hash})
-	return true
-}
-
-func DeleteUser(id uint) bool {
-	var user DAO.User
-	if err := config.DB.Where(&DAO.User{ID: id}).Delete(&user).Error; err != nil {
-		return false
-	}
-	return true
-}
-
 func GetUser(id uint) (bool, DAO.User) {
 	var user DAO.User
 	if err := config.DB.Where(&DAO.User{ID: id}).First(&user).Error; err != nil {
@@ -52,17 +35,4 @@ func GetUserByUsername(username string) (bool, DAO.User) {
 		return false, user
 	}
 	return true, user
-}
-
-func GetAllUsers(page uint) ([]DAO.User, error) {
-	var users []DAO.User
-	var user DAO.User
-	offset := (page - 1) * utils.PaginationLimit
-	queryBuilder := config.DB.Limit(utils.PaginationLimit).Offset(offset)
-	result := queryBuilder.Model(&DAO.User{}).Where(user).Find(&users)
-	if result.Error != nil {
-		msg := result.Error
-		return nil, msg
-	}
-	return users, nil
 }
